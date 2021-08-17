74317b6254da4c3d475ce39c9c09a420786c4193
Weslley Ferreira <weslley.ferreira108@hotmail.com>
2021-08-16 22:22:35 -0300

    melhoria primeiro exercicio

--- Exercicio01/Exercicio01.java
+++ Exercicio01/Exercicio01.java
@@ -7,16 +7,19 @@ public class Exercicio01 {
         Scanner sc = new Scanner(System.in);
 
         int idade;
-        int soma = 0;
         int ate14 = 0;
         int idadeFaixa1 = 0;
         int idadeFaixa2 = 0;
         int idadeFaixa3 = 0;
         int idadeAcima60 = 0;
+        int totalPessoas = 0;
+        double porcentagem;
+        double porcentagemAcima60;
 
 
 
-        for (int i =0; i <= 10; i++){
+
+        for (int i =1; i <= 10; i++){
             System.out.print("Digite a idade: ");
             idade = sc.nextInt();
             if (idade <= 14){
@@ -39,12 +42,23 @@ public class Exercicio01 {
                 idadeAcima60 += 1;
             }
 
+
         }
+
+        totalPessoas = ate14 + idadeFaixa1 + idadeFaixa2 + idadeFaixa3 + idadeAcima60;
+        porcentagem = (double) ate14 / totalPessoas * 100;
+
+        porcentagemAcima60 = (double) idadeAcima60 / totalPessoas * 100;
+
+        System.out.println();
         System.out.println("Até 14 anos: " + ate14);
         System.out.println("De 15 a 29 anos " + idadeFaixa1);
         System.out.println("De 30 a 44 anos " + idadeFaixa2);
         System.out.println("De 45 a 59 anos " + idadeFaixa3);
         System.out.println("Acima de 60 anos " + idadeAcima60);
+        System.out.println("Total de pessoas " + totalPessoas);
+        System.out.printf("Porcentagem de pessoas até 14 anos =  %.2f%n ",  porcentagem);
+        System.out.printf("Porcentagem de pessoas acima de 60 = %.2f%n ",  porcentagemAcima60);
 
     }
 }
